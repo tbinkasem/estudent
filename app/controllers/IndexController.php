3dc7425e83fe60204d67cc51b84e10743417f89a
Qserver <Qserver@Qserver-PC>
2014-05-11 16:30:17 +0700

    add

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -2,6 +2,6 @@
 class IndexController extends \Phalcon\Mvc\Controller {
 	public function indexAction() {
 		$user = new Users ();
-		$this->view->items = Users::find(); //ดึงข้อมูลจากตาราง users
+		$this->view->items = Users::find(); //ดึงข้อมูลจากตาราง users  
 	}
 }
